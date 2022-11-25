3548f35f37803692f0a26ed3d107e61ffb224998
gabrieldias <gabrielrosadias@gmail.com>
2022-11-24 22:27:29 -0300

    refactor: rm class PortasLogicas and add and class

---
+++ src/neander/And.java
@@ -0,0 +1,18 @@
+package neander;
+
+public class And {
+
+	public And() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	int and(int a, int b) {
+		return a&b;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
