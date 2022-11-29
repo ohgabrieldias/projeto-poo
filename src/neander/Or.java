d75c32d55889c5bdb6ec81b3b6d25dd7e6b3fd9f
Samuel Garcia <31748265+SamGularte@users.noreply.github.com>
2022-11-29 14:28:45 -0300

    or adicionado

---
+++ src/neander/Or.java
@@ -0,0 +1,16 @@
+package neander;
+
+public class Or{
+
+	public Or() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	int or(int a, int b) {
+		if(a == 1 || b == 1)
+      {
+        return 1;
+      }
+    return 0;
+	}
+  
