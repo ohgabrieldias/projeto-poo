46274f7b09d2ea54597e4829b078683be122aff7
TPD01 <119019018+TPD01@users.noreply.github.com>
2022-12-15 16:20:41 -0300

    Update Xor.java

--- src/neander/Xor.java
+++ src/neander/Xor.java
@@ -3,9 +3,9 @@ package neander;
 public class Xor {  
     public static void main(String[] args)
     {
-        boolean a = true;
-        boolean b = false;
-        boolean c = true;
+        int a = true;
+        int b = false;
+        int c = true;
 
         System.out.println(a^b);
         System.out.println(b^c);
