8b17cf07c7edc723e374cbf34d9cd3a79e315849
TPD01 <119019018+TPD01@users.noreply.github.com>
2022-12-15 16:07:21 -0300

    Create Xor.java

---
+++ src/neander/Xor.java
@@ -0,0 +1,13 @@
+public class Main
+{  
+    public static void main(String[] args)
+    {
+        boolean a = true;
+        boolean b = false;
+        boolean c = true;
+
+        System.out.println(a^b);
+        System.out.println(b^c);
+        System.out.println(c^a);
+    }
+}
