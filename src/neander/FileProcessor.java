03029df9e6da0354574479baaa1d593e37554a5b
Samuel Garcia <31748265+SamGularte@users.noreply.github.com>
2023-02-07 15:50:38 -0300

    Update FileProcessor.java

--- src/neander/FileProcessor.java
+++ src/neander/FileProcessor.java
@@ -33,128 +33,127 @@ public class FileProcessor {
 		
 	}
 	
-	public void extrairDados() {
-		matrix = new int[contaLinhas()][8]; //matriz de 8 bits
-//      Scanner input = new Scanner(System.in);
-//	      System.out.println(contaLinhas());
-	      String fileName = "te.txt";
-        try {
-            Scanner file = new Scanner(new File(fileName));
-            int lineCounter = 0;
-            while (file.hasNextLine()) {
-                String line = file.nextLine();
+  public void extrairDados() {
+    matrix = new int[contaLinhas()][8]; // matriz de 8 bits
+    // Scanner input = new Scanner(System.in);
+    // System.out.println(contaLinhas());
+    int aux = -1;
+    boolean resetou = false;
+    String fileName = "te.txt";
+    try {
+      Scanner file = new Scanner(new File(fileName));
+      int lineCounter = 0;
+      while (file.hasNextLine()) {
+        String line = file.nextLine();
 
-                if (line.length() > 3) {
-                    String[] parts = line.split(" ");
-                    String mnemonic = parts[0];
-                    
-                    int decimal = Integer.parseInt(parts[1]);
-                    String tmp = mnemonic;
+        if (line.length() > 3) {
+          String[] parts = line.split(" ");
+          String mnemonic = parts[0];
 
-                    switch (tmp) {
-                        case "STA":
-                            matrix[lineCounter][0] = 0;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 0;
-                            matrix[lineCounter][3] = 1;
-                            break;
-                        case "LDA":
-                            matrix[lineCounter][0] = 0;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 1;
-                            matrix[lineCounter][3] = 0;
-                            break;
-                    
-                        case "ADD":
-                            matrix[lineCounter][0] = 0;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 1;
-                            matrix[lineCounter][3] = 1;
-                            break;
-                        case "OR":
-                            matrix[lineCounter][0] = 0;
-                            matrix[lineCounter][1] = 1;
-                            matrix[lineCounter][2] = 0;
-                            matrix[lineCounter][3] = 0;
-                            break;
-                        case "AND":
-                            matrix[lineCounter][0] = 0;
-                            matrix[lineCounter][1] = 1;
-                            matrix[lineCounter][2] = 0;
-                            matrix[lineCounter][3] = 1;
-                            break;
-                        case "JMP":
-                            matrix[lineCounter][0] = 1;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 0;
-                            matrix[lineCounter][3] = 0;
-                            break;
-                        case "JN":
-                            matrix[lineCounter][0] = 1;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 0;
-                            matrix[lineCounter][3] = 1;
-                            break;
-                        case "JZ":
-                            matrix[lineCounter][0] = 1;
-                            matrix[lineCounter][1] = 0;
-                            matrix[lineCounter][2] = 1;
-                            matrix[lineCounter][3] = 0;
-                            break;
-                    } 
+          aux += 1;
 
-                    lineCounter++;
+          int decimal = Integer.parseInt(parts[1]);
+          String tmp = mnemonic;
 
-                    int i = 7;
-		
-                    while(decimal > 0){    
-                        matrix[lineCounter][i--] = decimal%2;    
-                        decimal = decimal/2;    
-                    }
-                } 
-                else 
-                {
-                    if(line.equals("NOP"))
-                    {
-                        matrix[lineCounter][0] = 0;
-                        matrix[lineCounter][1] = 0;
-                        matrix[lineCounter][2] = 0;
-                        matrix[lineCounter][3] = 0;
-                    }
-                    else if(line.equals("HLT"))
-                    {
-                        matrix[lineCounter][0] = 1;
-                        matrix[lineCounter][1] = 1;
-                        matrix[lineCounter][2] = 1;
-                        matrix[lineCounter][3] = 1;
-                    }
-                    else if(line.equals("NOT"))
-                    {
-                        matrix[lineCounter][0] = 0;
-                        matrix[lineCounter][1] = 1;
-                        matrix[lineCounter][2] = 1;
-                        matrix[lineCounter][3] = 0;
-                    }
-                    else{
-                        int decimal = Integer.parseInt(line);
-                        int i = 7;
-		
-                        while(decimal > 0){    
-                            matrix[lineCounter][i--] = decimal%2;    
-                            decimal = decimal/2;    
-                        }
-                }
-              }
-              lineCounter++;
-//              System.out.println("nvo");
+          switch (tmp) {
+            case "STA":
+              matrix[lineCounter][0] = 0;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 0;
+              matrix[lineCounter][3] = 1;
+              break;
+            case "LDA":
+              matrix[lineCounter][0] = 0;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 1;
+              matrix[lineCounter][3] = 0;
+              break;
+
+            case "ADD":
+              matrix[lineCounter][0] = 0;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 1;
+              matrix[lineCounter][3] = 1;
+              break;
+            case "OR":
+              matrix[lineCounter][0] = 0;
+              matrix[lineCounter][1] = 1;
+              matrix[lineCounter][2] = 0;
+              matrix[lineCounter][3] = 0;
+              break;
+            case "AND":
+              matrix[lineCounter][0] = 0;
+              matrix[lineCounter][1] = 1;
+              matrix[lineCounter][2] = 0;
+              matrix[lineCounter][3] = 1;
+              break;
+            case "JMP":
+              matrix[lineCounter][0] = 1;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 0;
+              matrix[lineCounter][3] = 0;
+              break;
+            case "JN":
+              matrix[lineCounter][0] = 1;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 0;
+              matrix[lineCounter][3] = 1;
+              break;
+            case "JZ":
+              matrix[lineCounter][0] = 1;
+              matrix[lineCounter][1] = 0;
+              matrix[lineCounter][2] = 1;
+              matrix[lineCounter][3] = 0;
+              break;
           }
-          
-          
 
-      } catch (FileNotFoundException e) {
-          System.out.println("Error: File not found");
+          lineCounter++;
+
+          int i = 7;
+
+          while (decimal > 0) {
+            matrix[lineCounter][i--] = decimal % 2;
+            decimal = decimal / 2;
+          }
+        } else {
+          if ((lineCounter == 128) && !(resetou)) {
+            lineCounter = lineCounter - aux;
+            resetou = true;
+          }
+
+          if (line.equals("NOP")) {
+            matrix[lineCounter][0] = 0;
+            matrix[lineCounter][1] = 0;
+            matrix[lineCounter][2] = 0;
+            matrix[lineCounter][3] = 0;
+          } else if (line.equals("HLT")) {
+            matrix[lineCounter][0] = 1;
+            matrix[lineCounter][1] = 1;
+            matrix[lineCounter][2] = 1;
+            matrix[lineCounter][3] = 1;
+          } else if (line.equals("NOT")) {
+            matrix[lineCounter][0] = 0;
+            matrix[lineCounter][1] = 1;
+            matrix[lineCounter][2] = 1;
+            matrix[lineCounter][3] = 0;
+          } else {
+            int decimal = Integer.parseInt(line);
+            int i = 7;
+
+            while (decimal > 0) {
+              matrix[lineCounter][i--] = decimal % 2;
+              decimal = decimal / 2;
+            }
+          }
+        }
+        lineCounter++;
+        // System.out.println("nvo");
       }
-	}
+
+    } catch (FileNotFoundException e) {
+      System.out.println("Error: File not found");
+    }
+  }
 	
 	public void imprimir() {
 		for (int i = 0; i < matrix.length; i++) {
