9a71f7f3cb095e87ffbade51e0ee9cf7591a3f00
TPD01 <119019018+TPD01@users.noreply.github.com>
2023-02-07 16:33:07 -0300

    Update Xor.java

--- src/neander/Xor.java
+++ src/neander/Xor.java
@@ -1,14 +1,12 @@
 package neander;
 
-public class Xor {  
-    public static void main(String[] args)
-    {
-//        int a = true;
-//        int b = false;
-//        int c = true;
-//
-//        System.out.println(a^b);
-//        System.out.println(b^c);
-//        System.out.println(c^a);
-    }
+public class Xor {
+
+  public Xor() {
+    // TODO Auto-generated constructor stub
+  }
+
+  int xor(int a, int b) {
+    return (a == b) ? 0 : 1;
+  }
 }
